d19b9f433d70bd98aed694299f74778b7fd6782e
Subbusainath <34021963+subbusainath@users.noreply.github.com>
2021-10-03 00:36:21 +0530

    initial commit

--- lib/medium-cdk-blog-post-stack.ts
+++ lib/medium-cdk-blog-post-stack.ts
@@ -1,9 +1,25 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as iam from "@aws-cdk/aws-iam";
 
 export class MediumCdkBlogPostStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here
+    const mydemobucket = new s3.Bucket(this, "my-demo-blog-bucket", {
+      bucketName: "my-demo-blog-bucket-1",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // adding bucket policy for this bucket
+    mydemobucket.addToResourcePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        principals: [new iam.ServicePrincipal("lambda.amazonaws.com")],
+        actions: ["s3:GetObject"],
+        resources: [`${mydemobucket.bucketArn}/*`],
+      })
+    );
   }
 }
